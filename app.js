199b2a250d3a5fdff6e21187e9059beda2dd95d0
andrew-merz <merz.school@gmail.com>
2022-04-05 10:55:07 -0400

    html core done

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
+// let hunger = document.querySelector(".hunger");
+// let boredom = document.querySelectorAll(".boredom");
+// let sleepiness = document.querySelector(".boredom");
 
+// class Status {
+//   constructor(hunger, boredom, sleepiness, age) {
+//     this.hunger = 5;
+//     this.boredom = 6;
+//     this.sleepiness = 7;
+//     this.age = age;
+//   }
+// }
+// console.log(this.hunger);
+
+const hungry = {
+  feedMe() {
+    const foodTimer = setInterval(() => {
+      console.log("hi");
+    }, 2000);
+  },
+};
+console.log(hungry);
