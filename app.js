ab4c93073495f229631e763bb125196889716b74
andrew-merz <merz.school@gmail.com>
2022-04-05 14:45:37 -0400

    Timers and Age working but not well

--- app.js
+++ app.js
@@ -1,22 +1,59 @@
-// let hunger = document.querySelector(".hunger");
-// let boredom = document.querySelectorAll(".boredom");
-// let sleepiness = document.querySelector(".boredom");
+//Status Bars
+let hunger = document.querySelector(".hunger");
+let boredom = document.querySelector(".boredom");
+let sleepiness = document.querySelector(".sleepiness");
+let age = document.querySelector(".age");
+//Buttons
+let feedBtn = document.querySelector(".feed");
+let boredBtn = document.querySelector(".play");
+let sleepBtn = document.querySelector(".lights");
+let startBtn = document.querySelector(".start");
 
-// class Status {
-//   constructor(hunger, boredom, sleepiness, age) {
-//     this.hunger = 5;
-//     this.boredom = 6;
-//     this.sleepiness = 7;
-//     this.age = age;
-//   }
-// }
-// console.log(this.hunger);
+//Starting Status
+let hungerStatus = 10;
+let lightStatus = 15;
+let boredStatus = 8;
+let ageStatus = 1;
 
-const hungry = {
-  feedMe() {
+function feedMe() {
+  if (hungerStatus > 0) {
     const foodTimer = setInterval(() => {
-      console.log("hi");
+      hunger.innerHTML = `Hunger: ${hungerStatus--}`;
     }, 2000);
-  },
-};
-console.log(hungry);
+  } else if (hungerStatus === 0) {
+    hunger.innerHTML = `Hunger: 0`;
+  }
+}
+
+function lightSwitch() {
+  if (lightStatus > 0) {
+    const sleepTimer = setInterval(() => {
+      sleepiness.innerHTML = `Sleepiness: ${lightStatus--}`;
+    }, 2000);
+  } else if (hungerStatus === 0) {
+    hunger.innerHTML = `Hunger: 0`;
+  }
+}
+
+function playWith() {
+  if (boredStatus > 0) {
+    const playTimer = setInterval(() => {
+      boredom.innerHTML = `Boredom: ${boredStatus--}`;
+    }, 1000);
+  } else if (hungerStatus === 0) {
+    hunger.innerHTML = `Hunger: 0`;
+  }
+}
+
+function aging() {
+  const ageTimer = setInterval(() => {
+    age.innerHTML = `Age: ${ageStatus++}`;
+  }, 10000);
+}
+
+startBtn.addEventListener("click", () => {
+  feedMe();
+  playWith();
+  lightSwitch();
+  aging();
+});
